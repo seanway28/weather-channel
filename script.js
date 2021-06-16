809b8f803a8a516ce263b09433cdd1a7ee43bd23
Sean Conway <81600120+seanway28@users.noreply.github.com>
2021-06-16 15:06:17 -0400

    jj is gone

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var cities = [];
 var cityFormEl=document.querySelector("#city-search-form");
 var cityInputEl=document.querySelector("#city");
 var weatherContainerEl=document.querySelector("#current-weather-container");
-var citySearchInputEl = document.querySelector("#searched-city");
+var citySearchInputEl = document.querySelector("#searched-city");""
 var forecastTitle = document.querySelector("#forecast");
 var forecastContainerEl = document.querySelector("#fiveday-container");
 var pastSearchButtonEl = document.querySelector("#past-search-buttons");
@@ -22,7 +22,7 @@ var formSumbitHandler = function(event){
     saveSearch();
     pastSearch(city);
 }
-jj
+
 var saveSearch = function(){
     localStorage.setItem("cities", JSON.stringify(cities));
 };
